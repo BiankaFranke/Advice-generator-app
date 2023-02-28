87d96f37c966725dee2c5a50a5e20ad7f94cc90d
“BiankaFranke” <bianka_franke@hotmail.de>
2023-02-28 19:44:23 +0100

    final mobile design

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+console.log('it works');
+
+// DECLARATIONS
+const url = 'https://api.adviceslip.com/advice';
+const dice = document.querySelector('#dice');
+let advice = document.querySelector('#advice');
+let number = document.querySelector('#adviceNo');
+
+// FUNCTION
+
+let giveMeAnAdvice = (e) => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        let randomNo = data['slip']['id'];
+        let randomAdvice = data['slip']['advice'];
+
+        number.innerText = randomNo;
+        advice.innerText = randomAdvice;
+});
+}
+
+// EVENTLISTENER
+dice.addEventListener('click', (e) => {
+    e.preventDefault();
+    giveMeAnAdvice();
+});
